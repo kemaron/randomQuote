51423470716a7711278489e6fa46cf1e48668ade
Daniel Balcer <kemaron@gmail.com>
2019-03-21 00:21:47 +0100

    Add createTweet

--- js/script.js
+++ js/script.js
@@ -1,28 +1,55 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+
 function getQuote() {
-    console.log('jest tu');
     fetch(quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
-            console.log ('jest');
-        });
-        //.then(createTweet);
+        })
+        .then(createTweet);
 }
 
 function createTweet(input) {
     var data = input[0];
+    console.log ('data: ' + data);
 
     var dataElement = document.createElement('div');
+    console.log ('dataElement: ' + dataElement);
     dataElement.innerHTML = data.content;
+    console.log ('dataContent: ' + data.content);
     var quoteText = dataElement.innerText.trim();
+    console.log ('quoteText: ' + dataElement);
     var quoteAuthor = data.title;
+    console.log ('quoteAuthor: ' + quoteAuthor);
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    }
 }
 
-getQuote();
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
+});
+
+/*
+console.log (fetch(quoteUrl, { cache: "no-store" })
+.then(function(resp) {
+    return resp.json();
+}));
+
+*/
 
